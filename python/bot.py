448412d0efdd80c3d131ed065c3f68530017e992
William Roberge <202059891@edu.clg.qc.ca>
2023-10-22 19:47:36 -0400

    aim bot first version

--- python/bot.py
+++ python/bot.py
@@ -2,10 +2,15 @@ from game_message import *
 from actions import *
 
 
+
 class Bot:
     Hauteur = 0
     Largeur = 0
-    MiLageur = 0
+    MiHauteur = 0
+    InfosAsteroide = 0
+    PosCanon = 0
+    VitesseMissile = 0
+
     def __init__(self):
         self.direction = 1
         print("Initializing your super mega duper bot")
@@ -19,15 +24,15 @@ class Bot:
             self.firstTick(game_message)
 
         Target = game_message.meteors[0]
-        TargetInterest = 0
+        TargetInterestValue = 0
         for meteor in game_message.meteors:
            print(meteor.id)
-           meteorInterest =  self.InterestIndext(meteor.position.y, meteor.position.x)
-           if(meteorInterest>TargetInterest):
+           meteorInterest =  self.InterestIndext(game_message, meteor.position.y, meteor.position.x, meteor.meteorType)
+           if(meteorInterest>TargetInterestValue):
                Target = meteor
-               TargetInterest = meteorInterest
+               TargetInterestValue = meteorInterest
 
-        position = Vector(x=int(Target.position.x), y=int(Target.position.y))
+        position = self.AimBot(Target)
 
         return [LookAtAction(position),ShootAction(),]
 
@@ -50,15 +55,57 @@ class Bot:
     def firstTick(self, game_message: GameMessage):
         self.Hauteur = game_message.constants.world.height
         self.Largeur = game_message.constants.world.width
-        self.MiLageur = self.Largeur/2
+        self.MiHauteur = self.Hauteur/2
+        self.InfosAsteroide = game_message.constants.meteorInfos
+        self.PosCanon = game_message.cannon.position
+        self.VitesseMissile = game_message.constants.rockets.speed
 
 
-    def InterestIndext(self, HauteurActuel, LargeurActuel):
+    def InterestIndext(self,gamemessage ,HauteurActuel, LargeurActuel, TypeAsteroide):
 
-        proportionHauteur = HauteurActuel/self.Hauteur
-        valeurHauteur = proportionHauteur /1
+        proportionLargeur =  (LargeurActuel/self.Largeur)
 
-        distValMilieu = abs(LargeurActuel - self.MiLageur)
-        valeuLargeur = distValMilieu/1
 
-        return valeuLargeur + valeurHauteur
+        distValMilieu = abs(HauteurActuel - self.MiHauteur) /3
+        distValMilieu = 1
+
+
+        facteur = 1
+        if(LargeurActuel < 120):
+            facteur = 0
+        elif(TypeAsteroide == MeteorType.Large):
+            facteur = 0.9
+        elif(TypeAsteroide == MeteorType.Medium):
+            facteur = 0.72
+        elif (TypeAsteroide == MeteorType.Small ):
+            facteur = 0.8
+
+        else:
+            facteur = 0.7
+
+        return (distValMilieu + proportionLargeur) * facteur
+
+    def AimBot(self,  AsteroideCible : Meteor):
+        diffVitesse = self.soustractionVecteur(AsteroideCible.velocity , Vector(x=self.VitesseMissile/1.90, y=self.VitesseMissile/1.2))
+        diffPosition = self.soustractionVecteur(self.PosCanon, AsteroideCible.position)
+        TempsColision = self.produitScalaire(diffPosition, diffVitesse) / self.produitScalaire(diffVitesse, diffVitesse)
+
+        posIntercept = self.additionVecteur(AsteroideCible.position, self.multiplicationVecteur(AsteroideCible.velocity, TempsColision))
+
+        return posIntercept
+
+
+    def produitScalaire(self,v1, v2):
+        return v1.x * v2.x + v1.y * v2.y
+
+
+    def soustractionVecteur(self, v1, v2):
+        return Vector(v1.x - v2.x, v1.y - v2.y)
+
+
+    def additionVecteur(self,v1, v2):
+        return Vector(v1.x + v2.x, v1.y + v2.y)
+
+
+    def multiplicationVecteur(self, v, scalaire):
+        return Vector(v.x * scalaire, v.y * scalaire)
