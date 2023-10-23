ff326600e31dde28ad102a9dc2b33174eaffd82d
William Roberge <202059891@edu.clg.qc.ca>
2023-10-23 00:45:16 -0400

    aim bot V2

--- python/bot.py
+++ python/bot.py
@@ -23,35 +23,31 @@ class Bot:
         if(self.tick == 0):
             self.firstTick(game_message)
 
+
         Target = game_message.meteors[0]
+        Target2 = game_message.meteors[0]
+        Target3 = game_message.meteors[0]
+        Target4 = game_message.meteors[0]
+
         TargetInterestValue = 0
         for meteor in game_message.meteors:
-           print(meteor.id)
            meteorInterest =  self.InterestIndext(game_message, meteor.position.y, meteor.position.x, meteor.meteorType)
            if(meteorInterest>TargetInterestValue):
+               Target4 = Target3
+               Target3 = Target2
+               Target2 = Target
                Target = meteor
                TargetInterestValue = meteorInterest
 
-        position = self.AimBot(Target)
+        positionTarget = self.AimBot(Target)
+        positionTarget2 = self.AimBot(Target2)
+        positionTarget3 = self.AimBot(Target3)
+        positionTarget4 = self.AimBot(Target4)
+        print(game_message.score)
 
-        return [LookAtAction(position),ShootAction(),]
+        return [LookAtAction(positionTarget),ShootAction(),LookAtAction(positionTarget2),ShootAction(),LookAtAction(positionTarget3),ShootAction(),LookAtAction(positionTarget4),ShootAction(),]
 
 
-        """
-        Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
-        """
-        """
-
-        if game_message.cannon.orientation >= 45:
-            self.direction = -1
-        elif game_message.cannon.orientation <= -45:
-            self.direction = 1
-
-        return [
-            RotateAction(angle=15 * self.direction),
-            ShootAction(),
-        ]
-        """
     def firstTick(self, game_message: GameMessage):
         self.Hauteur = game_message.constants.world.height
         self.Largeur = game_message.constants.world.width
@@ -63,22 +59,29 @@ class Bot:
 
     def InterestIndext(self,gamemessage ,HauteurActuel, LargeurActuel, TypeAsteroide):
 
-        proportionLargeur =  (LargeurActuel/self.Largeur)
+        proportionLargeur =  (LargeurActuel/self.Largeur) *1.5
 
 
-        distValMilieu = abs(HauteurActuel - self.MiHauteur) /3
-        distValMilieu = 1
+        distValMilieu = 1.5* (1/ abs(HauteurActuel - self.MiHauteur))
+        #distValMilieu = 1
 
 
         facteur = 1
-        if(LargeurActuel < 200):
+        if(LargeurActuel < 40):
             facteur = 0
-        elif (TypeAsteroide == MeteorType.Small and LargeurActuel < 600):
-            facteur = 1.2
+        elif (TypeAsteroide == MeteorType.Small and LargeurActuel < self.Largeur/2 and HauteurActuel > (self.Hauteur /5) and HauteurActuel < (4*self.Hauteur /5)):
+            facteur = 16
+        elif (TypeAsteroide == MeteorType.Small and LargeurActuel > self.Largeur/2  ):
+            facteur = 0.3
+        elif (TypeAsteroide == MeteorType.Medium and  HauteurActuel > (self.Hauteur / 3) and HauteurActuel < (2 * (self.Hauteur / 3))):
+            facteur = 10
+        elif (TypeAsteroide == MeteorType.Large and LargeurActuel > 2*self.Largeur/3):
+            facteur = 3
+
         elif(TypeAsteroide == MeteorType.Large):
             facteur = 0.9
         elif(TypeAsteroide == MeteorType.Medium):
-            facteur = 0.72
+            facteur = 1.2
         elif (TypeAsteroide == MeteorType.Small ):
             facteur = 0.9
 
@@ -97,10 +100,31 @@ class Bot:
         return posIntercept
 
     def volicityApproxyMissil(self, AsteroideCible : Meteor):
-        VecteurDirection = self.soustractionVecteur(AsteroideCible.position, self.PosCanon)
+        positionEstimee = AsteroideCible.position
+
+        for _ in range(70):  # 10 itérations pour convergence (ajuster si nécessaire)
+            vecteur_vitesse_missile = self.volicityApproxyMissil_vers_position(positionEstimee)
+            delta_temps = self.tempsImpact(positionEstimee, self.PosCanon, self.norme(vecteur_vitesse_missile))
+            positionEstimee = self.estimerPosition(AsteroideCible.position, AsteroideCible.velocity, delta_temps)
+        return self.volicityApproxyMissil_vers_position(positionEstimee)
+
+    def volicityApproxyMissil_vers_position(self, position):
+        """Version originale de la fonction pour obtenir la vélocité du missile vers une position donnée."""
+        VecteurDirection = self.soustractionVecteur(position, self.PosCanon)
         longueur = self.norme(VecteurDirection)
         VecteurUnitaire = Vector(VecteurDirection.x / longueur, VecteurDirection.y / longueur)
         return Vector(self.VitesseMissile * VecteurUnitaire.x, self.VitesseMissile * VecteurUnitaire.y)
+
+    def estimerPosition(self,positionAsteroide, vitesseAsteroide, delta_temps):
+        """Estime la nouvelle position de l'astéroïde après un certain temps."""
+        return Vector(positionAsteroide.x + vitesseAsteroide.x * delta_temps,
+                      positionAsteroide.y + vitesseAsteroide.y * delta_temps)
+
+    def tempsImpact(self,position_cible, position_lanceur, vitesse_missile):
+        """Estime le temps nécessaire pour que le missile atteigne la cible."""
+        vecteur_direction = self.soustractionVecteur(position_cible, position_lanceur)
+        longueur = self.norme(vecteur_direction)
+        return longueur / vitesse_missile
 
     def norme(self,v):
         return (v.x**2 + v.y**2)**0.5
